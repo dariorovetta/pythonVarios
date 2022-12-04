2b70ff89540900f7c51e966e45cafce27ef6d5e4
Dario Rovetta <102047493+dariorovetta@users.noreply.github.com>
2022-12-04 19:11:18 -0300

    Update listaPorComprension.py

--- listaPorComprension.py
+++ listaPorComprension.py
@@ -5,3 +5,15 @@ enteros = [1, 2, 3, 4, 5]
 enteros_mas_uno = [numero + 1 for numero in enteros]
 
 print(enteros_mas_uno)
+
+# Además, optativamente, también es posible establecer condiciones que nos permitan filtrar
+# los elementos dentro del iterable. Para ello, naturalmente, debemos incluir un llamado al
+# if luego de declarar al iterable:
+
+# definimos una lista
+enteros = [1, 2, 3, 4, 5]
+
+# definimos una lista por comprensión
+enteros_mas_uno = [numero + 1  for  numero  in  enteros if numero > 3]
+
+print(enteros_mas_uno)
